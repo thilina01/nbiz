4958fed8e348c2e95c48f213d8c07814824aed6f
r.thilina@gmail.com <r.thilina@gmail.com@400b1a76-c718-1faa-068d-fbc2fd2e6caf>
2013-10-05 03:34:40 +0000

    User Login function implemented

---
+++ Login/src/com/nanosl/nbiz/login/Installer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nanosl.nbiz.login;
+
+import com.nanosl.nbiz.login.gui.LoginPanel;
+import org.openide.modules.ModuleInstall;
+
+public class Installer extends ModuleInstall {
+
+    private LoginPanel loginPanel = new LoginPanel();
+
+    @Override
+    public void restored() {
+        loginPanel.createLoginDialog();
+
+    }
+}
