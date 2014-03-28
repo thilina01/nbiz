79eb90e8745dc676e02656b3c2e9f1a0fa96cd6a
r.thilina@gmail.com <r.thilina@gmail.com@400b1a76-c718-1faa-068d-fbc2fd2e6caf>
2014-03-28 20:16:42 +0000

    Functionality improvements

---
+++ trunk/db/src/entity/Config.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Thilina
+ */
+@Entity
+@Table(name = "config")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Config.findAll", query = "SELECT c FROM Config c"),
+    @NamedQuery(name = "Config.findByConfigKey", query = "SELECT c FROM Config c WHERE c.configKey = :configKey"),
+    @NamedQuery(name = "Config.findByConfigValue", query = "SELECT c FROM Config c WHERE c.configValue = :configValue")})
+public class Config implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "config_key")
+    private String configKey;
+    @Column(name = "config_value")
+    private String configValue;
+
+    public Config() {
+    }
+
+    public Config(String configKey) {
+        this.configKey = configKey;
+    }
+
+    public String getConfigKey() {
+        return configKey;
+    }
+
+    public void setConfigKey(String configKey) {
+        this.configKey = configKey;
+    }
+
+    public String getConfigValue() {
+        return configValue;
+    }
+
+    public void setConfigValue(String configValue) {
+        this.configValue = configValue;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (configKey != null ? configKey.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Config)) {
+            return false;
+        }
+        Config other = (Config) object;
+        if ((this.configKey == null && other.configKey != null) || (this.configKey != null && !this.configKey.equals(other.configKey))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Config[ configKey=" + configKey + " ]";
+    }
+    
+}
