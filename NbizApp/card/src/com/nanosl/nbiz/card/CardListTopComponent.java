4f4c516335353a75f2c3fe68c1ae06fea77d0a69
r.thilina@gmail.com <r.thilina@gmail.com@400b1a76-c718-1faa-068d-fbc2fd2e6caf>
2014-12-30 15:03:16 +0000

    ==Card load cards of given range

--- NbizApp/card/src/com/nanosl/nbiz/card/CardListTopComponent.java
+++ NbizApp/card/src/com/nanosl/nbiz/card/CardListTopComponent.java
@@ -69,6 +69,10 @@ public final class CardListTopComponent extends NTopComponent {
         jLabel1 = new javax.swing.JLabel();
         totalAmountTextField = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        rangeStartTextField = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        rangeEndTextField = new javax.swing.JTextField();
 
         cardTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -112,6 +116,14 @@ public final class CardListTopComponent extends NTopComponent {
 
         org.openide.awt.Mnemonics.setLocalizedText(jLabel2, org.openide.util.NbBundle.getMessage(CardListTopComponent.class, "CardListTopComponent.jLabel2.text")); // NOI18N
 
+        org.openide.awt.Mnemonics.setLocalizedText(jLabel3, org.openide.util.NbBundle.getMessage(CardListTopComponent.class, "CardListTopComponent.jLabel3.text")); // NOI18N
+
+        rangeStartTextField.setText(org.openide.util.NbBundle.getMessage(CardListTopComponent.class, "CardListTopComponent.rangeStartTextField.text")); // NOI18N
+
+        org.openide.awt.Mnemonics.setLocalizedText(jLabel4, org.openide.util.NbBundle.getMessage(CardListTopComponent.class, "CardListTopComponent.jLabel4.text")); // NOI18N
+
+        rangeEndTextField.setText(org.openide.util.NbBundle.getMessage(CardListTopComponent.class, "CardListTopComponent.rangeEndTextField.text")); // NOI18N
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -119,11 +131,19 @@ public final class CardListTopComponent extends NTopComponent {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 787, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1081, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(loadButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(outstandingCheckBox)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(rangeStartTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(rangeEndTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -137,6 +157,8 @@ public final class CardListTopComponent extends NTopComponent {
 
         jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {totalAmountTextField, totalRemainingTextField});
 
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {rangeEndTextField, rangeStartTextField});
+
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
@@ -147,7 +169,11 @@ public final class CardListTopComponent extends NTopComponent {
                     .addComponent(totalRemainingTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1)
                     .addComponent(totalAmountTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(rangeStartTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4)
+                    .addComponent(rangeEndTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 442, Short.MAX_VALUE)
                 .addContainerGap())
@@ -172,7 +198,16 @@ public final class CardListTopComponent extends NTopComponent {
     }// </editor-fold>//GEN-END:initComponents
 
     private void loadButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loadButtonActionPerformed
-        Query query = manager.getEm().createQuery("SELECT s FROM SaleInvoice s WHERE s.cardNumber IS NOT NULL AND s.credit > 0");
+        String rangeStartText = rangeStartTextField.getText().trim();
+        String rangeEndText = rangeEndTextField.getText().trim();
+        String rangeQuery = "";
+        if (!rangeStartText.equals("")) {
+            rangeEndText = rangeEndText.equals("") ? rangeStartText : rangeEndText;
+            rangeQuery = " AND s.cardNumber BETWEEN " + rangeStartText + " AND " + rangeEndText + " ";
+        }
+        String allCreditQuery = "SELECT s FROM SaleInvoice s WHERE s.cardNumber IS NOT NULL AND s.credit > 0" + rangeQuery;
+
+        Query query = manager.getEm().createQuery(allCreditQuery);
         Collection<SaleInvoice> saleInvoices = outstandingCheckBox.isSelected() ? query.getResultList() : Find.saleInvoicesAsCard();
         tableModel.setRowCount(0);
         totalAmountTextField.setText("0.00");
@@ -199,10 +234,14 @@ public final class CardListTopComponent extends NTopComponent {
     private javax.swing.JTable cardTable;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton loadButton;
     private javax.swing.JCheckBox outstandingCheckBox;
+    private javax.swing.JTextField rangeEndTextField;
+    private javax.swing.JTextField rangeStartTextField;
     private javax.swing.JTextField totalAmountTextField;
     private javax.swing.JTextField totalRemainingTextField;
     // End of variables declaration//GEN-END:variables
